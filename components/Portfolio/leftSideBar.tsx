11c7000e3a9c78b2fc87b2db2411f9d1e71e7d84
catlense <catlense@voronin.xyz>
2022-05-22 01:31:26 +0700

    edit detail portfolio

--- components/Portfolio/leftSideBar.tsx
+++ components/Portfolio/leftSideBar.tsx
@@ -7,7 +7,7 @@ const LeftSideBar = ({data}:any) => {
       <img src={data?.img} alt="Project logotype" />
       <h2>{data?.subTitle}</h2>
       <a href={`https://${data?.title}`} rel="noreferrer" target="_blank">{data?.title}</a>
-      <p>Заказчик: <a href={data?.customer.href} target="_blank" rel="noreferrer">{data?.customer.text}</a></p>
+      <p>Дата разработки: {data?.date}</p>
       <a style={{color: 'white'}} href={data?.customLink || `https://${data?.title}`} rel="noreferrer" target="_blank"><button style={{background: data?.specColor}}>Открыть проект</button></a>
     </div>
   )
